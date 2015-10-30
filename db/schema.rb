f16c2fa34e9ac0488a7cb044ae9501139156f287
maangie <maangie906@gmail.com>
2015-10-30 10:11:40 +0900

    add a index.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029105008) do
+ActiveRecord::Schema.define(version: 20151030010816) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20151029105008) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title",            null: false
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -9,6 +9,10 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
+# Indexes
+#
+#  index_answers_on_question_id  (question_id)
+#
 
 require 'rails_helper'
 
