fdd28bdebe33f5f5b38769fbdfaa9c38e88718ab
maangie <maangie906@gmail.com>
2015-10-30 09:37:51 +0900

    fix type of "Publish" column "publish_datetime" to datetime

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029073022) do
+ActiveRecord::Schema.define(version: 20151029105008) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 20151029073022) do
   create_table "questions", force: :cascade do |t|
     t.string   "title",            null: false
     t.string   "image"
-    t.string   "publish_datetime"
+    t.datetime "publish_datetime"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
