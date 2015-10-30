d5f604d7606a681d868e9c283dbfc4813b058c1b
maangie <maangie906@gmail.com>
2015-10-30 12:43:17 +0900

    edit "Question" administration page.

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -1,17 +1,29 @@
 ActiveAdmin.register Question do
+  # See permitted parameters documentation:
+  # http://bit.ly/1ZWXppB
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
 
-# See permitted parameters documentation:
-# http://bit.ly/1ZWXppB
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+  permit_params(%i(title image image_cache publish_datetime_date
+                   publish_datetime_time_hour publish_datetime_time_minute) +
+                [answers_attributes: %i(id answer_text total_count _destroy)])
 
-  permit_params :title, :image, :image_cache, :publish_datetime
+  form(html: { multipart: true }) do |f|
+    f.inputs 'Details' do
+      f.input :title
+      f.input :image
+      f.input :publish_datetime, as: :just_datetime_picker
+      f.input :image_cache, as: :hidden
+    end
+
+    f.actions
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -19,6 +19,8 @@ class Question < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
+  just_define_datetime_picker :publish_datetime
+
   validates :title,            presence: true
   validates :publish_datetime, presence: true
   validates :image,            presence: true
