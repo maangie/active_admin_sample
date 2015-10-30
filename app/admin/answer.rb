7c5c081cb21c4b03bd9e8de3e2bb59ce11e746bf
maangie <maangie906@gmail.com>
2015-10-30 13:42:02 +0900

    answer input.

--- app/admin/answer.rb
+++ app/admin/answer.rb
@@ -11,4 +11,14 @@ ActiveAdmin.register Answer do
   #   permitted << :other if resource.something?
   #   permitted
   # end
+
+  permit_params %(question_id answer_text total_count)
+
+  form do |f|
+    f.inputs 'Details' do
+      f.input :question
+      f.input :answer_text
+      f.input :total_count
+    end
+  end
 end
