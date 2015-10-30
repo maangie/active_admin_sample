302ec0ffc7381162a671ebda6da1013aaae74d0a
maangie <maangie906@gmail.com>
2015-10-30 10:26:00 +0900

    edit uploader.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 
+# 画像アップローダ
 class ImageUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -40,15 +40,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb
   # for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
+  def filename
+    'image' + File.extname(original_filename) if original_filename
+  end
 end
